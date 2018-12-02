6b537bced4d1c48b0d61972a3b7ae8f3a1ea3f0e
Simone Stefani <s.stefani95@gmail.com>
2018-12-02 16:12:46 +0200

    Complete challenge 2

--- 01-frequencey-changes/frequency-changes.ex
+++ 01-frequencey-changes/frequency-changes.ex
@@ -9,6 +9,24 @@ defmodule FrequencyChanges do
 
   def compute_changes(file) do
     read(file)
-    |> Enum.reduce(fn val, acc -> val + acc end)
+    |> Enum.sum
+  end
+
+  def first_repeated_frequency(file) do
+    values = read(file)
+    first_repeated_frequency(values, values, [], 0)
+  end
+
+  def first_repeated_frequency(values, [h | t], freqs, acc) do
+    case Enum.member?(freqs, acc) do
+      true -> acc
+      false ->
+        case t do
+          [] -> 
+            first_repeated_frequency(values, values, [acc | freqs], acc + h)
+          _ ->
+            first_repeated_frequency(values, t, [acc | freqs], acc + h)
+        end
+    end
   end
 end
