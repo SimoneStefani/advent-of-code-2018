9c375c7d38bc875a75f7137cbeb14185ba3786e8
Simone Stefani <s.stefani95@gmail.com>
2018-12-10 23:54:14 +0200

    Complete challenge 3 part 2

--- 03-overlapping-tiles/overlapping-tiles.ex
+++ 03-overlapping-tiles/overlapping-tiles.ex
@@ -13,10 +13,8 @@ defmodule OverlappingTiles do
     |> Enum.map(&String.to_integer/1)
   end
 
-  def compute_claimed_tiles(claims) do
-    Enum.reduce(claims, %{}, fn claim, acc ->
-      [id, left, top, width, height] = parse_input(claim)
-
+  def compute_claimed_tiles(parsed_claims) do
+    Enum.reduce(parsed_claims, %{}, fn [id, left, top, width, height], acc ->
       Enum.reduce((left + 1)..(left + width), acc, fn x, acc ->
         Enum.reduce((top + 1)..(top + height), acc, fn y, acc ->
           Map.update(acc, {x, y}, [id], &[id | &1])
@@ -29,9 +27,31 @@ defmodule OverlappingTiles do
     for {coord, [_, _ | _]} <- compute_claimed_tiles(claims), do: coord
   end
 
+  def compute_non_overlapping_tile(parsed_claims) do
+    overlap = compute_claimed_tiles(parsed_claims)
+
+    [id, _, _, _, _] =
+      Enum.find(parsed_claims, fn [id, left, top, width, height] ->
+        Enum.all?((left + 1)..(left + width), fn x ->
+          Enum.all?((top + 1)..(top + height), fn y ->
+            Map.get(overlap, {x, y}) == [id]
+          end)
+        end)
+      end)
+    
+    id
+  end
+
   def count_overlapping_claims(file) do
     read(file)
+    |> Enum.map(&parse_input/1)
     |> compute_overlapped_tiles
     |> length
+  end
+
+  def find_non_overlapping_claim(file) do
+    read(file)
+    |> Enum.map(&parse_input/1)
+    |> compute_non_overlapping_tile
   end
 end
